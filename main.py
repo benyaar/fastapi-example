13465c26af562510bdfaa645b5dc2dbdfe8fb966
Artur <82472015+benyaar@users.noreply.github.com>
2025-08-27 23:50:28 +0300

    connect bot

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from typing import Union
-
 from fastapi import FastAPI
+from typing import Union
+import asyncio
+from app.bot import start_bot
 
 app = FastAPI()
-
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+# Запускаємо бота у фоні разом із FastAPI
+@app.on_event("startup")
+async def on_startup():
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_bot())
